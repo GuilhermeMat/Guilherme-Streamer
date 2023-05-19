9e494857501af7f19c0f70b6d1ff9a037fcd6da2
Guilherme de Matos Silva <127167230+GuilhermeMat@users.noreply.github.com>
2023-05-19 16:26:14 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guilherme_Mat_
+Este projeto tem por finalidade mostear a funcionalidade de um slide automático usando HTML, CSS e JS
+![Captura de tela 2023-05-18 213231](https://github.com/GuilhermeMat/Guilherme_Mat_/assets/127167230/09743047-4936-4b8b-be25-e1bdd61b544b)
---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Essa funcão tem como finalidade passar os slides de forma automática
+let contador = 1;
+
+setInterval(function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador ++;
+    if(contador > 5){
+        contador = 1;
+    }
+},6000);
